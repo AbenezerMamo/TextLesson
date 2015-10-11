6b9b28659d5e23e8ea03e85ae3d6eaad0ca4be21
AbenezerMamo <ElitePowered@gmail.com>
2015-10-10 17:52:05 -0700

    testing db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 
@@ -19,6 +20,15 @@ db.create_all()
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
 api_manager.create_api(Lesson, methods=['GET', 'POST', 'DELETE', 'PUT'])
 
+def get_response():
+    response = r = requests.get('https://localhost:5000/api/lesson/1')
+    return response.json()[0]
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    resp = twilio.twiml.Response()
+    resp.message(get_response())
+    return str(resp)
 
 
 if __name__ == "__main__":
