1fae725ccec30e08514fb0b4d8a371c1a1392439
AbenezerMamo <ElitePowered@gmail.com>
2015-10-10 17:53:04 -0700

    fixed twilio logic

--- app.py
+++ app.py
@@ -20,16 +20,5 @@ db.create_all()
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
 api_manager.create_api(Lesson, methods=['GET', 'POST', 'DELETE', 'PUT'])
 
-def get_response():
-    response = r = requests.get('https://localhost:5000/api/lesson/1')
-    return response.json()[0]
-
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    resp = twilio.twiml.Response()
-    resp.message(get_response())
-    return str(resp)
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
